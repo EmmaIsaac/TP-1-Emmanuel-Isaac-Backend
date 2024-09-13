70e3da0fa810b38ec17aabff00f7b2f183b55ddb
EmmaIsaac <emmanuel.i157@yahoo.com.ar>
2024-09-13 18:47:57 -0300

    add: addUser

--- models.js
+++ models.js
@@ -53,16 +53,54 @@ const getUserById = (id) => {
   }
 };
 
-// addUser recibe un objeto con toda la data para el nuevo usuario
-// valida que esten los datos míminos para añadir un nuevo usuario
-// valida que el nombre sea un string
-// valida que el apellido sea un string
-// valida que el email sea un string y que no se repita
-// hashea la contraseña antes de registrar al usuario
+
 const addUser = (userData) => {
   try {
-  } catch (error) {}
+    const {nombre, apellido, email, password} =  userData;
+    
+    if (!nombre || !apellido || !email || !password) { 
+      throw new Error("Missing data");
+    }
+    
+    if ((typeof nombre !== "string") || (typeof apellido !== "string") || (typeof email !== "string")){
+      throw new Error("Data not string");
+    }
+
+    if (!email.includes("@")) {
+      throw new Error("Invalid Email");
+    }
+
+    const users = getUsers(PATH_FILE_USER);
+
+    const findEmail = users.find((user) => user.email === email);
+
+    if (findEmail) {
+      throw new Error("Email already exists");
+    }
+
+    const hash = createHash("sha256").update(password).digest("hex")
+    
+    const newUser = 
+      {
+        id: randomUUID(),
+        nombre,
+        apellido,
+        email,
+        password: hash,
+        isLoggedIn: false
+    }
+    
+    users.push(newUser);
+
+    writeFileSync(PATH_FILE_USER, JSON.stringify(users));
+    return newUser;
+
+  } catch (error) {
+    const objError = handleError(error, PATH_FILE_ERROR);
+    return objError;
+  }
 };
+
 
 // todos los datos del usuario seleccionado se podrían modificar menos el ID
 // si se modifica la pass debería ser nuevamente hasheada

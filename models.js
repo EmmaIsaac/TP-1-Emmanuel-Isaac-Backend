e321d81c362d9a590ab7b551b325d9351eb49269
EmmaIsaac <emmanuel.i157@yahoo.com.ar>
2024-09-14 18:58:43 -0300

    add: updateUser

--- models.js
+++ models.js
@@ -75,7 +75,7 @@ const addUser = (userData) => {
     const findEmail = users.find((user) => user.email === email);
 
     if (findEmail) {
-      throw new Error("Email already exists");
+      throw new Error("Email already exists. Try another email.");
     }
 
     const hash = createHash("sha256").update(password).digest("hex")
@@ -107,8 +107,65 @@ const addUser = (userData) => {
 // si se modifica el email, validar que este no exista
 const updateUser = (userData) => {
   try {
-  } catch (error) {}
+    const {id, nombre, apellido, email, password} =  userData;
+
+    if (!id) {
+      throw new Error("ID is missing");
+    }
+
+    if (!nombre || !apellido || !email || !password) { 
+      throw new Error("Missing data");
+    }
+    
+    if ((typeof nombre !== "string") || (typeof apellido !== "string") || (typeof email !== "string")){
+      throw new Error("Data not string");
+    }
+
+    if (!email.includes("@")) {
+      throw new Error("Invalid Email");
+    }
+
+    const users = getUsers(PATH_FILE_USER);
+    //Filtro la lista de usuarios excluyendo al id cargado
+    const filteredUsers = users.filter((user) => user.id !== id);
+    //Con la lista filtrada, busco que otra cuenta no este usando el email que se quiere actualizar
+    const foundEmail = filteredUsers.find((user) => user.email === email);
+    //Si otra cuenta posee el email, arroja el error
+    if (foundEmail) {
+      throw new Error("Email already exists. Try another email.")
+    }
+
+    const foundUser = users.find((user) => user.id === id);
+
+    if (!foundUser) {
+      throw new Error("User not found");
+    }
+
+    if (nombre) foundUser.nombre = nombre;
+    if (apellido) foundUser.apellido = apellido;
+    if (email) foundUser.email = email;
+    const hash = createHash("sha256").update(password).digest("hex")
+    if (password) foundUser.password = hash;
+
+    writeFileSync(PATH_FILE_USER, JSON.stringify(users));
+    return foundUser;
+
+  } catch (error) {
+    const objError = handleError(error, PATH_FILE_ERROR);
+    return objError;
+  }
 };
+
+// const userToUpdate = {
+//   id:"1",
+//   nombre:"Juan pablo",
+//   apellido:"Pérez con Pan",
+//   email:"juan.perez@example.com",
+//   password:"contraseña123567"
+// };
+
+// const respuesta = updateUser(userToUpdate);
+// console.log(respuesta);
 
 const deleteUser = (id) => {
   try {
